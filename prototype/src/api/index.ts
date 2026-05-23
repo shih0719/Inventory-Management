f303743ee934fe07ffc4725124964ccf4f1ca79c
eric <yourenshih@gmail.com>
2026-05-23 18:57:22 +0800

    delete prototype(old design)

--- prototype/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { apiClient } from './client';
-export * from './products';
-export * from './transactions';
-export * from './batches';
-export * from './tags';
-export * from './locations';
